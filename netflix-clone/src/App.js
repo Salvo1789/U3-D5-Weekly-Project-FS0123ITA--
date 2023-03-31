4c3d9d6a25e1a8d9e15d9131025fcf2a6bff2474
Salvo1789 <salvo.mercurio89@gmail.com>
2023-03-31 13:12:54 +0200

    new navbar and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NetflixNav from "./components/NetflixNav"
+import NetflixFooter from "./components/NetflixFooter"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NetflixNav />
+    <NetflixFooter />
+    </>
   );
 }
 
