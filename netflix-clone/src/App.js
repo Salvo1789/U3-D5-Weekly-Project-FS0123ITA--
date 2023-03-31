7581c69fba32b24332e698f61b82e876e960d82a
Salvo1789 <salvo.mercurio89@gmail.com>
2023-03-31 21:55:32 +0200

    New user profile react component

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -5,6 +5,7 @@ import NetflixGallery1 from "./components/NetflixGallery1"
 import NetflixGallery2 from "./components/NetflixGallery2"
 import NetflixGallery3 from "./components/NetflixGallery3"
 import NetflixFooter from "./components/NetflixFooter"
+import NetflixProfile from './components/NetflixProfile';
 
 function App() {
   return (
@@ -17,5 +18,6 @@ function App() {
     </>
   );
 }
+// inserire in Return <NetflixProfile/> come unico componente per check
 
 export default App;
