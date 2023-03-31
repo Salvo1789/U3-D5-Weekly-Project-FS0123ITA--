cd2849767a1e4ad5238078c6405d1bfc4d42375d
Salvo1789 <salvo.mercurio89@gmail.com>
2023-03-31 16:50:51 +0200

    Added three galleries

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 import NetflixNav from "./components/NetflixNav"
+import NetflixGallery1 from "./components/NetflixGallery1"
+import NetflixGallery2 from "./components/NetflixGallery2"
+import NetflixGallery3 from "./components/NetflixGallery3"
 import NetflixFooter from "./components/NetflixFooter"
 
 function App() {
   return (
     <>
     <NetflixNav />
+    <NetflixGallery1/>
+    <NetflixGallery2/>
+    <NetflixGallery3/>
     <NetflixFooter />
     </>
   );
